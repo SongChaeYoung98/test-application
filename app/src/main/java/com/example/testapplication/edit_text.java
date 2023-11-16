bfcc8edfdbb36e8d39f8866037395d4555ddd579
SongChaeYoung98 <0.0yeriel@gmail.com>
2023-11-16 17:14:42 +0900

    [UPDATE] 과제 5
    6일차 (23.11.27)
    Author : SongChaeYoung98
    2023-11-16 17:14

--- app/src/main/java/com/example/testapplication/edit_text.java
+++ app/src/main/java/com/example/testapplication/edit_text.java
@@ -1,6 +1,13 @@
 package com.example.testapplication;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +17,30 @@ public class edit_text extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_text);
+
+        // 과제 5
+        Button close_button = findViewById(R.id.edit_text_close_button);
+        LinearLayout parent_layout = findViewById(R.id.edit_text_main_layout);
+        EditText name_editText = findViewById(R.id.edit_Text_name_editText);
+
+        // 과제 5
+        close_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        parent_layout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                name_editText.clearFocus();
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                return false;
+            }
+        });
     }
 }
