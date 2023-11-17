c3f66667a141b35d31a1226d84b1de1e2e208122
SongChaeYoung98 <0.0yeriel@gmail.com>
2023-11-17 13:38:56 +0900

    [UPDATE] 과제 10
    12일차 (23.12.05)
    Author : SongChaeYoung98
    2023-11-17 13:38

--- app/src/main/java/com/example/testapplication/edit_text.java
+++ app/src/main/java/com/example/testapplication/edit_text.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -32,6 +34,9 @@ public class edit_text extends AppCompatActivity {
         EditText email_editText = findViewById(R.id.edit_text_email_editText);
         EditText email_num_editText = findViewById(R.id.edit_text_email_num_editText);
 
+        // 과제 10
+        Button visible_button = findViewById(R.id.edit_text_visible_button);
+
         // 과제 5
         close_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +45,7 @@ public class edit_text extends AppCompatActivity {
             }
         });
 
-        // 과제 9
+
         phone_editText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -51,7 +56,14 @@ public class edit_text extends AppCompatActivity {
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 String inputEmail = charSequence.toString();
 
-                Log.d("test", "input : " + inputEmail);
+                Log.d("test", "input : " + inputEmail);     // 과제 9
+
+                // 과제 10
+                if (inputEmail.isEmpty()) {
+                    visible_button.setVisibility(View.INVISIBLE);
+                } else {
+                    visible_button.setVisibility(View.VISIBLE);
+                }
 
             }
 
@@ -60,6 +72,33 @@ public class edit_text extends AppCompatActivity {
 
             }
         });
+
+        // 과제 10
+        visible_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (phone_editText.getText().toString().isEmpty()) {
+                    return;
+                }
+
+                int currentVisibility = phone_editText.getTransformationMethod() == PasswordTransformationMethod.getInstance() ? View.GONE : View.VISIBLE;
+
+                phone_editText.setTransformationMethod(
+                        phone_editText.getTransformationMethod() == PasswordTransformationMethod.getInstance()
+                                ? HideReturnsTransformationMethod.getInstance()
+                                : PasswordTransformationMethod.getInstance()
+                );
+
+                if (currentVisibility == View.VISIBLE) {
+                    visible_button.setBackgroundResource(R.drawable.btn_visible);
+                } else {
+                    visible_button.setBackgroundResource(R.drawable.btn_invisible);
+                }
+
+                phone_editText.setSelection(phone_editText.getText().length());
+            }
+        });
+
 
         parent_layout.setOnTouchListener(new View.OnTouchListener() {
             @Override
